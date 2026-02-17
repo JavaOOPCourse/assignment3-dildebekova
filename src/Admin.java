2bb2bc23e720e467c483aced79131f3b37482c0e
nazima <dildebekvass@gmail.com>
2026-02-17 21:30:20 +0600

    Implement Admin service management methods

--- src/Admin.java
+++ src/Admin.java
@@ -1,22 +1,20 @@
+// Admin.java
 public class Admin extends User {
-
-    // TODO: constructor
-
+    public Admin(String name, int userId) {
+        super(name, userId);
+    }
 
     @Override
     public void manageService(Service s) {
-
-        // TODO:
-        // print admin managing message
-
+        System.out.println("🔧 Admin '" + name + "' managing service: '" + s.getServiceName() + "'");
+        if (!s.isActive()) {
+            System.out.println("   → Service is currently inactive.");
+        }
     }
 
     @Override
     public void useService(Service s) {
-
-        // TODO:
-        // print admin using message
-        // call performService()
-
+        System.out.println("⚙️ Admin '" + name + "' accessing service: '" + s.getServiceName() + "'");
+        s.performService();
     }
 }
